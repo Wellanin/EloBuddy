4db89e6e980a8321302ee5766304acc76ffcffbb
Wellanin <spyder_onin@yahoo.com>
2015-10-08 11:04:38 +0800

    first

---
+++ Sona_EB/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("EB Sona")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("EB Sona")]
+[assembly: AssemblyCopyright ("Copyright © 2015")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: Guid ("e703181e-02fa-4daa-bb82-c6ecf5982671")]
